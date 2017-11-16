ddfa7373c472fae901cc99bfb27c3f8cc558a96a
Peter Jang <peter.x.jang@gmail.com>
2017-11-15 21:27:02 -0600

    Add RESTful show action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,8 +4,8 @@ class RecipesController < ApplicationController
     render json: recipes.as_json
   end
 
-  def one_recipe_method
-    recipe_id = params["input_recipe_id"]
+  def show
+    recipe_id = params["id"]
     recipe = Recipe.find_by(id: recipe_id)
     render json: recipe.as_json
   end
