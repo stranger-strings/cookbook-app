5bb4d6306d912a4ff6bcd9b8ded2afd1066bdefd
Peter Jang <peter.x.jang@gmail.com>
2017-11-15 21:12:12 -0600

    Add examples for displaying single recipe using params

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,7 @@
 class RecipesController < ApplicationController
   def one_recipe_method
-    recipe = Recipe.second
+    recipe_id = params["input_recipe_id"]
+    recipe = Recipe.find_by(id: recipe_id)
     render json: recipe.as_json
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/one_recipe_url" => "recipes#one_recipe_method"
+  get "/one_recipe_url_segment_url/:input_recipe_id" => "recipes#one_recipe_method"
 end
