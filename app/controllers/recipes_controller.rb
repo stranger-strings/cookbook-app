8d232bf52758dba1f4e49969e00fa5219325c061
Peter Jang <peter.x.jang@gmail.com>
2017-11-15 21:15:03 -0600

    Add RESTful index action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,9 @@
 class RecipesController < ApplicationController
+  def index
+    recipes = Recipe.all
+    render json: recipes.as_json
+  end
+
   def one_recipe_method
     recipe_id = params["input_recipe_id"]
     recipe = Recipe.find_by(id: recipe_id)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/recipes" => "recipes#index"
+
   get "/one_recipe_url" => "recipes#one_recipe_method"
   get "/one_recipe_url_segment_url/:input_recipe_id" => "recipes#one_recipe_method"
 end
