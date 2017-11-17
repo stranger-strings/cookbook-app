1ad96cff866b392292f865e90ed3c20f40f67018
Peter Jang <peter.x.jang@gmail.com>
2017-11-16 20:16:55 -0600

    Add simple update action with frontend code

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -20,4 +20,15 @@ class RecipesController < ApplicationController
     recipe = Recipe.find_by(id: recipe_id)
     render json: recipe.as_json
   end
+
+  def update
+    recipe_id = params["id"]
+    recipe = Recipe.find_by(id: recipe_id)
+    recipe.title = params["input_title"]
+    recipe.chef = params["input_chef"]
+    recipe.ingredients = params["input_ingredients"]
+    recipe.directions = params["input_directions"]
+    recipe.save
+    render json: recipe.as_json
+  end
 end
--- frontend.rb
+++ frontend.rb
@@ -6,6 +6,7 @@ puts "Welcome to the Recipe app! Select an option:"
 puts "[1] See all recipes"
 puts "[2] Create a recipe"
 puts "[3] See one recipe"
+puts "[4] Update a recipe"
 
 input_option = gets.chomp
 
@@ -30,6 +31,21 @@ elsif input_option == "3"
   print "Which recipe id do you want to see? "
   recipe_id = gets.chomp
   response = Unirest.get("http://localhost:3000/recipes/#{recipe_id}")
+  recipe = response.body
+  pp recipe
+elsif input_option == "4"
+  print "Which recipe id do you want to update? "
+  recipe_id = gets.chomp
+  params = {}
+  print "Enter the updated recipe title: "
+  params["input_title"] = gets.chomp
+  print "Enter the updated recipe chef: "
+  params["input_chef"] = gets.chomp
+  print "Enter the updated recipe ingredients: "
+  params["input_ingredients"] = gets.chomp
+  print "Enter the updated recipe directions: "
+  params["input_directions"] = gets.chomp
+  response = Unirest.patch("http://localhost:3000/recipes/#{recipe_id}", parameters: params)
   recipe = response.body
   pp recipe
 end
