0fdc52167d149a8808f2fa3b10a46118b8836e30
Peter Jang <peter.x.jang@gmail.com>
2017-11-16 18:34:27 -0600

    Add sample frontend script (with simple create example)

---
+++ frontend.rb
@@ -0,0 +1,26 @@
+require "unirest"
+require "pp"
+
+system "clear"
+puts "Welcome to the Recipe app! Select an option:"
+puts "[1] See all recipes"
+puts "[2] Create a recipe"
+puts "[3] See one recipe"
+
+input_option = gets.chomp
+
+if input_option == "1"
+  response = Unirest.get("http://localhost:3000/recipes")
+  recipes = response.body
+  pp recipes
+elsif input_option == "2"
+  response = Unirest.post("http://localhost:3000/recipes?input_title=Test&input_chef=Peter&input_ingredients=Salt&input_directions=Salty")
+  recipe = response.body
+  pp recipe
+elsif input_option == "3"
+  print "Which recipe id do you want to see? "
+  recipe_id = gets.chomp
+  response = Unirest.get("http://localhost:3000/recipes/#{recipe_id}")
+  recipe = response.body
+  pp recipe
+end
