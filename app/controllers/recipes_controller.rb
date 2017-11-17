98e4e01299690f1a1485e9aecbef993773aa469c
Peter Jang <peter.x.jang@gmail.com>
2017-11-16 18:23:21 -0600

    Add RESTful create action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,6 +4,17 @@ class RecipesController < ApplicationController
     render json: recipes.as_json
   end
 
+  def create
+    recipe = Recipe.new(
+      title: params["input_title"],
+      chef: params["input_chef"],
+      ingredients: params["input_ingredients"],
+      directions: params["input_directions"]
+    )
+    recipe.save
+    render json: recipe.as_json
+  end
+
   def show
     recipe_id = params["id"]
     recipe = Recipe.find_by(id: recipe_id)
