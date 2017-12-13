a30915506717e6b6e04065a1cd5b57983f347967
Peter Jang <peter.x.jang@gmail.com>
2017-12-12 19:15:34 -0600

    Use JavaScript web request (AJAX) to display each recipe

--- public/index.js
+++ public/index.js
@@ -1,8 +1,20 @@
-var template = document.querySelector("#recipe-card");
-var recipeContainer = document.querySelector(".row");
+/* global axios */
 
-recipeContainer.appendChild(template.content.cloneNode(true));
-recipeContainer.appendChild(template.content.cloneNode(true));
-recipeContainer.appendChild(template.content.cloneNode(true));
-recipeContainer.appendChild(template.content.cloneNode(true));
-recipeContainer.appendChild(template.content.cloneNode(true));
+axios.get("http://localhost:3000/recipes").then(function(response) {
+  var recipes = response.data;
+  console.log(recipes);
+
+  var template = document.querySelector("#recipe-card");
+  var recipeContainer = document.querySelector(".row");
+
+  recipes.forEach(function(recipe) {
+    var clone = template.content.cloneNode(true);
+
+    clone.querySelector(".card-title").innerText = recipe.title;
+    clone.querySelector(".card-img-top").src = recipe.image;
+    clone.querySelector(".ingredients").innerText = recipe.ingredients;
+    clone.querySelector(".directions").innerText = recipe.directions;
+
+    recipeContainer.appendChild(clone);
+  });
+});
