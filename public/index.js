b17cc38b0cd12e019c7cf5d360ebd5bc20e373aa
Peter Jang <peter.x.jang@gmail.com>
2017-12-12 18:25:43 -0600

    Use template with JavaScript to generate multiple recipes

---
+++ public/index.js
@@ -0,0 +1,8 @@
+var template = document.querySelector("#recipe-card");
+var recipeContainer = document.querySelector(".row");
+
+recipeContainer.appendChild(template.content.cloneNode(true));
+recipeContainer.appendChild(template.content.cloneNode(true));
+recipeContainer.appendChild(template.content.cloneNode(true));
+recipeContainer.appendChild(template.content.cloneNode(true));
+recipeContainer.appendChild(template.content.cloneNode(true));
