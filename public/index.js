dc38efdf303b06d37fa385c5b49e712e52e90f69
Peter Jang <peter.x.jang@gmail.com>
2017-12-18 20:51:12 -0600

    Convert frontend to Vue.js

--- public/index.js
+++ public/index.js
@@ -1,20 +1,29 @@
-/* global axios */
+/* global Vue, VueRouter, axios */
 
-axios.get("/recipes").then(function(response) {
-  var recipes = response.data;
-  console.log(recipes);
+var HomePage = {
+  template: "#home-page",
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      recipes: []
+    };
+  },
+  mounted: function() {
+    axios.get("/recipes").then(
+      function(response) {
+        this.recipes = response.data;
+      }.bind(this)
+    );
+  },
+  methods: {},
+  computed: {}
+};
 
-  var template = document.querySelector("#recipe-card");
-  var recipeContainer = document.querySelector(".row");
+var router = new VueRouter({
+  routes: [{ path: "/", component: HomePage }]
+});
 
-  recipes.forEach(function(recipe) {
-    var clone = template.content.cloneNode(true);
-
-    clone.querySelector(".card-title").innerText = recipe.title;
-    clone.querySelector(".card-img-top").src = recipe.image;
-    clone.querySelector(".ingredients").innerText = recipe.ingredients;
-    clone.querySelector(".directions").innerText = recipe.directions;
-
-    recipeContainer.appendChild(clone);
-  });
+var app = new Vue({
+  el: "#app",
+  router: router
 });
