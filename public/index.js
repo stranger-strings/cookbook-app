c28ff9e42f0d241ed07877855d483274d4611385
Peter Jang <peter.x.jang@gmail.com>
2017-12-18 21:20:30 -0600

    Add recipes show template with frontend route

--- public/index.js
+++ public/index.js
@@ -19,8 +19,33 @@ var HomePage = {
   computed: {}
 };
 
+var RecipesShowPage = {
+  template: "#recipes-show-page",
+  data: function() {
+    return {
+      recipe: {
+        title: "Sample recipe",
+        ingredients: ["Sample ingredient"],
+        directions: ["Sample directions"]
+      }
+    };
+  },
+  mounted: function() {
+    axios.get("/recipes/" + this.$route.params.id).then(
+      function(response) {
+        this.recipe = response.data;
+      }.bind(this)
+    );
+  },
+  methods: {},
+  computed: {}
+};
+
 var router = new VueRouter({
-  routes: [{ path: "/", component: HomePage }]
+  routes: [
+    { path: "/", component: HomePage },
+    { path: "/recipes/:id", component: RecipesShowPage }
+  ]
 });
 
 var app = new Vue({
